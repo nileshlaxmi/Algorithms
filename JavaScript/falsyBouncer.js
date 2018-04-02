e5382a5b2011cad38ed022100a14653b892855a2
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-04-02 18:45:40 +0530

    Deleted copy of falsy bouncer script

--- JavaScript/falsyBouncer.js
+++
@@ -1,9 +0,0 @@
-//Remove all falsy values from an array.
-
-function bouncer(arr) {
-  // Don't show a false ID to this bouncer.
-  arr = arr.filter(Boolean);
-  return arr;
-}
-
-bouncer([7, "ate", "", false, 9]);
