6fe245539736604c4881571db0dca6a3588a675a
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-04-15 00:17:34 +0530

    updated mutations.js

--- JavaScript/Basic/mutations.js
+++ JavaScript/Basic/mutations.js
@@ -1,69 +1,22 @@
 //Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
+
 function mutation(arr) {
+  var first = arr[0].toLowerCase();
+  var sec = arr[1].toLowerCase();
 
-  var first = arr[0].toLowerCase(), sec = arr[1].toLowerCase();
-  var fLength = first.length, sLength = sec.length;
-  var fArr = [], sArr = [], i, newAr = [];
-  if(fLength >= sLength){
-    i=0;
-    while(i < fLength){
-      fArr.push(first.substr(i,1));
-      sArr.push(sec.substr(i,1));
-      i++;
-    }
-    fArr = fArr.filter(Boolean);
-    sArr = sArr.filter(Boolean);
-    console.log(fArr);
-    console.log(sArr);
-    for(var j=0; j<fArr.length; j++){
-      newAr.push(sArr.indexOf(fArr[j]));
-    }
-    console.log(newAr);
-    for(var j=0; j<newAr.length; j++){
-      if(newAr.includes(-1)){
-        console.log(false);
-        return false;
-      }
-      else{
-        console.log(true);
-        return true;
-      }
-    }
-
-  }
-  else{
-    i=0;
-    while(i < sLength){
-      fArr.push(first.substr(i,1));
-      sArr.push(sec.substr(i,1));
-      i++;
-    }
-    fArr = fArr.filter(Boolean);
-    sArr = sArr.filter(Boolean);
-    console.log(fArr);
-    console.log(sArr);
-
-    for(var j=0; j<sArr.length; j++){
-      newAr.push(sArr.indexOf(fArr[j]));
-    }
-    console.log(newAr);
-    for(var j=0; j<newAr.length; j++){
-      if(newAr.includes(-1)){
-        console.log(false);
-        return false;
-      }
-      else{
-        console.log(true);
-        return true;
-      }
+  for(var i=0; i<sec.length; i++){
+    if(first.indexOf(sec[i]) === -1){
+		console.log(false);
+      return false;
     }
   }
-
-
-
-
+  console.log(true);
+  return true;
 }
+
+mutation(["hello", "hey"]);
+
 
 mutation(["hello", "Hey"]);
 mutation(["hello", "Hello"]);
