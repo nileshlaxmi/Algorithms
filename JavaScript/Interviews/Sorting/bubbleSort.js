0a9939a7079f568a7080598975bc8fd26dcd6f91
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-09 19:12:40 +0530

    Added Bubble Sorting Algorithm

---
+++ JavaScript/Interviews/Sorting/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(array) {
+
+for(let i=0; i<array.length; i++){
+    for(let j=0; j<array.length-1; j++){
+        if(array[j]>=array[j+1]){
+            let temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }
+    }
+}
+console.log(array);
+return array;
+}
+
+bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+bubbleSort([6, 4, 7, 3, 1, 2]);
+// test array:
+// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
