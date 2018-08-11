54a9aeea696f5b7edafe7971f9a5640d35c9ef2e
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-11 12:49:40 +0530

    Completed Linear Search

--- JavaScript/Interviews/Searching/linearSearch.js
+++ JavaScript/Interviews/Searching/linearSearch.js
@@ -1,5 +1,16 @@
 const linearSearch = (n, ar) => {
-
+    let c = 0;
+    for(let i=0; i< ar.length; i++){
+        if(n === ar[i]){
+            c++;
+        }
+    }
+    if(c>=1){
+        console.log("Number Found");
+    }
+    else{
+        console.log("Number not found");
+    }
 }
 
 linearSearch(5, [1,2,3,4,5])
