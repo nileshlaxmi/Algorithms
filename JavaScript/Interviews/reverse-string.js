d42ce6f3a50626f56b1f9c19f6e7d8ed42335b39
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-10-26 00:32:30 +0530

    Updated shortcut methods

--- JavaScript/Interviews/reverse-string.js
+++ JavaScript/Interviews/reverse-string.js
@@ -1,3 +1,6 @@
+/**
+ * Long Method
+ */
 // const reverseString = (str) => {
 //     let newStr = "";
 //     for(let i=str.length-1; i>=0; i--){
@@ -7,11 +10,25 @@
 //     console.log(str);
 // }
 
+/**
+ * Using Recursion
+ */
+// function reverseString(str){
+//     if(str === "")
+//         return "";
+//     else {
+//         return reverseString(str.substr(1)) + str.charAt(0);
+//     }
+// }
+
+/**
+ * Shortcut Method
+ */
 function reverseString(str){
     if(str === "")
         return "";
     else {
-        return reverseString(str.substr(1)) + str.charAt(0);
+        return str.split("").reverse().join("");
     }
 }
 
